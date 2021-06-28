3e0127cd982c024a2c18ee5a5cd2d1f1bb640a65
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-06-28 15:14:33 +0200

    Mise en place

---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="/" class="logo">PHP_Blog</a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+            <a href="./add-article.php">Écrire un article</a>
+        </li>
+    </ul>
+</header>
